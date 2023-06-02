e19e0dc6ba7d99a39f3ba28254877759abe26d69
abhithemachinegun <117830155+abhithemachinegun@users.noreply.github.com>
2023-06-02 14:24:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/188229bdb34805898f33d.mp4')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/aabcc8c7671a59b838f0a.mp4")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/9aae149859b51d281c90f.jpg")
-SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/9aae149859b51d281c90f.jpg")
+SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/e25b1e71c59831bc76672.mp4")
 
 # Admins, Channels & Users
 support_chat_id = environ.get('SUPPORT_CHAT_ID')
