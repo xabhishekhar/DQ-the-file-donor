03d86b4d80e7c90ddd28b6052c68934d70eef1a4
Abhishek <117830155+abhithemachinegun@users.noreply.github.com>
2023-07-16 14:43:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -353,7 +353,7 @@ async def language_check(bot, query):
         
         btn.insert(0, [
             InlineKeyboardButton("! Sᴇɴᴅ Aʟʟ Tᴏ PM !", url=f"https://t.me/{temp.U_NAME}"),
-            InlineKeyboardButton("✅ ᶜʰᵃⁿᵍᵉ ˡᵃⁿᵍᵘᵃᵍᵉ", callback_data=f"select_lang#{userid}")
+            InlineKeyboardButton("🔈Cʜᴀɴɢᴇ Lᴀɴɢᴜᴀɢᴇ ", callback_data=f"select_lang#{userid}")
         ])
 
         btn.insert(0, [
